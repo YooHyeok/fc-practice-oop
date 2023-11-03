b850132fa0b79e5609c8e89771c6b8ea912b3b63
webdevyoo <yjk7454@naver.com>
2023-11-03 20:10:00 +0900

    유효성 검증 프로덕션 validate메소드 리팩토링 (지역변수, 전역변수 추출)

--- src/main/java/org/example/PasswordValdator.java
+++ src/main/java/org/example/PasswordValdator.java
@@ -1,9 +1,11 @@
 package org.example;
 
 public class PasswordValdator {
+    public static final String WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE = "비밀번호는 최소 8자 이상 12자 이하여야 한다."; // 전역변수(상수) 추출 단축키 :Ctrl + Alt + C
     public static void validate(String password) {
-        if (password.length() < 8 || password.length() > 12) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+        int length = password.length(); // 지역변수 추출 단축키 : Ctrl + Alt + v (replace 2~...)
+        if (length < 8 || length > 12) {
+            throw new IllegalArgumentException(WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 }
--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -5,13 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
+
 /**
- * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
- * 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다.
- * 경계조건에 대해 테스트 코드를 작성해야 한다.
+ * 테스트코드를 작성하는 이유
+ * 1. 문서화 역활
+ * 2. 코드에 결함을 발견하기 위함
+ * 3. 리팩토링시 (심리적)안정성 확보
+ * 4. 테스트하기 쉬운 코드를 작성하다 보면 더 낮은 결합도를 가진 설계를 얻을 수 있음.
  */
 public class PasswordValidatorTest {
-    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.") //테스트코드의 의도를 작성
+    /**
+     * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
+     * 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다.
+     * 경계조건에 대해 테스트 코드를 작성해야 한다.
+     */
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.") // 테스트코드의 의도를 작성 - 1. 문서화 역활
     @Test
     void validatePasswordTest() {
         assertThatCode(() -> PasswordValdator.validate("serverwizard"))

b2e74183b023bb86ef4c96b26a449143e4c3e6dc
webdevyoo <yjk7454@naver.com>
2023-11-03 21:10:47 +0900

    User 프로덕션 클래스 추가 및 비밀번호 초기화 메소드 구현

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class User {
+    private String password;
+
+    /**
+     * 랜덤 패스워드의 길이가 8이상 12이하일 경우 password value를 랜덤패스워드 value로 초기화
+     */
+    public void initPassword() {
+
+        RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+        String randomPassword = randomPasswordGenerator.generatePassword();
+
+        /* 랜덤 패스워드의 길이가 8이상 12이하일 경우 password value를 랜덤패스워드 value로 초기화*/
+        if (randomPassword.length() >= 8 && randomPassword.length() <= 12) {
+            this.password = randomPassword;
+        }
+    }
+}

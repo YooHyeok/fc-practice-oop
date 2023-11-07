4385982a288d22bd72630866bfad015b9c62fd48
webdevyoo <yjk7454@naver.com>
2023-11-07 22:53:21 +0900

    MenuTest - Menu선택예외 테스트코드 작성

--- src/test/java/org/example/customer/MenuTest.java
+++ src/test/java/org/example/customer/MenuTest.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 
 public class MenuTest {
@@ -19,4 +20,12 @@ public class MenuTest {
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
     }
 
+    @DisplayName("메뉴판에 없는 메뉴를 선택할 시 예외를 반환한다.")
+    @Test
+    void chooseExceptionTest() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        assertThatCode(() -> menu.choose("라볶이"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 메뉴 이름입니다.");
+    }
 }
